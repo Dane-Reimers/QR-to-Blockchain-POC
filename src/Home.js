1a1de3e098d73dbfcc3be78115e044fe83a3a86e
Dane Reimers <dane.reimers@gmail.com>
2020-04-14 22:05:11 -0700

    Updated qr codes

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,20 @@ class Home extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
+          <h1>
             QR Codes
-          </p>
+          </h1>
+          <h2>QR Code 1</h2>
+          <img src={require("./QRCodes/QR1.png")}/>
+          <hr/>
+          <h2>QR Code 2</h2>
+          <img src={require("./QRCodes/QR2.png")}/>
+          <hr/>
+          <h2>QR Code 3</h2>
+          <img src={require("./QRCodes/QR3.png")}/>
+          <hr/>
+          <h2>QR Code 4</h2>
+          <img src={require("./QRCodes/QR4.png")}/>
         </header>
       </div>
     );
