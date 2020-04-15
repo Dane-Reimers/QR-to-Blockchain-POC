9c6ea5e9f0a0d116efdfa08c686c8718192b7f51
Dane Reimers <dane.reimers@gmail.com>
2020-04-14 22:19:25 -0700

    Moved blockchain interaction to qr class

--- src/App.js
+++ src/App.js
@@ -4,50 +4,11 @@ import {
   HashRouter,
   NavLink
 } from "react-router-dom";
-import { ethers } from "ethers";
 import Home from './Home.js';
 import QR from './QR.js';
 import './App.css';
-import {QR_CONTRACT_ADDRESS, QR_CONTRACT_ABI} from './config.js';
 
 class App extends Component {
-  componentDidMount() {
-    this.loadBlockchainData();
-  }
-
-  async loadBlockchainData() {
-    const Wallet = ethers.Wallet;
-    const Contract = ethers.Contract;
-    const utils = ethers.utils;
-    const providers = ethers.providers; 
-
-    //Note the "0x" appended at the start
-    let privateKey = process.env.REACT_APP_SERVER_WALLET_KEY;
-
-    let network = "ropsten";
-
-    //let network = "kovan";
-    //let network = "ropsten";
-    //let network = "rinkeby";
-    //let network = "homestead";
-
-    let infuraAPIKey = process.env.REACT_APP_INFURA_KEY;
-
-    //let provider = new providersJsonRpcProvider(network, 'homestead');
-    let provider = new providers.InfuraProvider(network, infuraAPIKey);
-    this.state.provider = provider;
-
-    let serverWallet = new Wallet(privateKey, provider);
-    this.state.serverWallet = serverWallet;
-
-    let contract = new ethers.Contract(QR_CONTRACT_ADDRESS, QR_CONTRACT_ABI, serverWallet)
-    this.state.contract = contract
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = { serverWallet: null, provider: null, contract: null }
-  }
 
   render() {
     return (
@@ -59,8 +20,7 @@ class App extends Component {
               }
             />
             <Route exact path="/qr/:id" render={props =>
-                this.state.contract && this.state.serverWallet &&
-                <QR contract={this.state.contract} serverWallet={this.state.serverWallet} {...props} />
+                <QR {...props} />
               }
             />
           </div>
--- src/QR.js
+++ src/QR.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
+import { ethers } from "ethers";
+import {QR_CONTRACT_ADDRESS, QR_CONTRACT_ABI} from './config.js';
  
 class QR extends Component {
 
   componentDidMount() {
-    this.scan().then(() => 
-    this.loadBlockchainData()
-    )
-  }
-
-  async scan() {
-    await this.props.contract.scan(this.state.id)
+    this.loadBlockchainData().then(() => 
+    this.scan().then(() =>
+    this.loadTimes()
+    ))
   }
 
   async loadBlockchainData() {
-    const timesScanned = await this.props.contract.getTimesScanned(this.state.id)
-    this.setState({ timesScanned: parseInt(timesScanned) })
+    const Wallet = ethers.Wallet;
+    const Contract = ethers.Contract;
+    const utils = ethers.utils;
+    const providers = ethers.providers; 
+
+    //Note the "0x" appended at the start
+    let privateKey = process.env.REACT_APP_SERVER_WALLET_KEY;
+
+    let network = "ropsten";
+
+    //let network = "kovan";
+    //let network = "ropsten";
+    //let network = "rinkeby";
+    //let network = "homestead";
+
+    let infuraAPIKey = process.env.REACT_APP_INFURA_KEY;
+
+    //let provider = new providersJsonRpcProvider(network, 'homestead');
+    let provider = await new providers.InfuraProvider(network, infuraAPIKey);
+    this.state.provider = provider;
+
+    let serverWallet = await new Wallet(privateKey, provider);
+    this.state.serverWallet = serverWallet;
+
+    let contract = await new ethers.Contract(QR_CONTRACT_ADDRESS, QR_CONTRACT_ABI, serverWallet)
+    this.state.contract = contract
   }
 
   constructor(props) {
     super(props)
     this.state = {
+      serverWallet: null,
+      provider: null,
+      contract: null,
       id: this.props.match.params.id,
       timesScanned: null
     }
+  }
+
+  async scan() {
+    await this.state.contract.scan(this.state.id)
+  }
+
+  async loadTimes() {
+    const timesScanned = await this.state.contract.getTimesScanned(this.state.id)
+    this.setState({ timesScanned: parseInt(timesScanned) })
   }
 
   render() {
